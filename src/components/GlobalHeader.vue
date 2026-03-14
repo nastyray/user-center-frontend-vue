8d41390a6429bdba9298ea29c9c156fb0eddd834
ray <lilray2002@163.com>
2026-03-14 17:26:48 +0800

    feat:初始化基本布局

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="globalHeader">
+    <a-menu v-model:selectedKeys="current" mode="horizontal" :items="items" />
+  </div>
+</template>
+<script lang="ts" setup>
+import { h, ref } from "vue";
+import { HomeOutlined, CrownOutlined } from "@ant-design/icons-vue";
+import { MenuProps } from "ant-design-vue";
+
+const current = ref<string[]>(["mail"]);
+const items = ref<MenuProps["items"]>([
+  {
+    key: "/",
+    icon: () => h(HomeOutlined),
+    label: "主页",
+    title: "主页",
+  },
+  {
+    key: "/user/login",
+    label: "用户登录",
+    title: "用户登录",
+  },
+  {
+    key: "/user/register",
+    label: "用户注册",
+    title: "用户注册",
+  },
+  {
+    key: "/user/userManage",
+    icon: () => h(CrownOutlined),
+    label: "用户管理",
+    title: "用户管理",
+  },
+  {
+    key: "others",
+    label: h(
+      "a",
+      { href: "https://www.codefather.cn", target: "_blank" },
+      "编程导航"
+    ),
+    title: "编程导航",
+  },
+]);
+</script>
--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,10 +1,44 @@
 <template>
-  <div id="basic-layout">
-
+  <div id="basicLayout">
+    <a-layout>
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+      <a-layout-footer class="footer">
+        <a href="https://github.com/nastyray" target="_blank"> lilray </a>
+      </a-layout-footer>
+    </a-layout>
   </div>
-
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
 
-<style scoped></style>
+<style scoped>
+#basicLayout .footer {
+  background: #efefef;
+  text-align: center;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  padding: 16px;
+}
+
+#basicLayout .content {
+  padding: 20px;
+  margin-bottom: 20px;
+  background: linear-gradient(to right, #fefefe, #fff);
+}
+
+#basicLayout .header {
+  background: white;
+  margin-bottom: 20px;
+  color: unset;
+  padding-inline: 20px;
+}
+</style>
